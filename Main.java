d312230e79c37cd17d82371adaa74cb2a57f40aa
Alicja Kałuża <92887428+alicja832@users.noreply.github.com>
2023-11-08 15:29:30 +0100

    Algorytm najkrotszej sciezki

--- Main.java
+++ Main.java
@@ -3,7 +3,9 @@ public class Main
     public static void main(String args[])
     {
         SolveMaze solve=new SolveMaze();
-        //solve.DisplayLabirynt();;
+        solve.DisplayLabirynt();
         solve.makePaths();
+        solve.backtrack();
+        solve.readWay();
     }
 }
